c16e2d2f38c2a4d2fd08f34fac5898c4868fffb1
Neil Killeen <nkilleen@unimelb.edu.au>
2017-09-12 18:36:46 +1000

    redeveloped from PET/CT as base model with Study date now parsed and used

--- daris-client-mbcmr-upload/src/main/java/nig/mf/mr/client/upload/MRMetaData.java
+++ daris-client-mbcmr-upload/src/main/java/nig/mf/mr/client/upload/MRMetaData.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Date;
+
 import nig.io.LittleEndianDataInputStream;
+import nig.util.DateUtil;
 
 /**
  * Siemens VB15-VB17 RaidFile structure
@@ -19,11 +22,12 @@ public class MRMetaData {
 	private String _lastName = null;
 	private String _patientID = null;
 	private String _fileName = null;
+	private Date _dateAndTime = null;
+	private String _ext = null;
 
 	public MRMetaData (File file) throws Throwable {
 		_file = file;
 		_fileName = _file.getName();
-
 	}
 	
 	
@@ -38,9 +42,18 @@ public class MRMetaData {
 	}
 	public String getLastName() {return _lastName;};
 	public String getID () {return _patientID;};
+	public Date getAcquisitionDateTime () {return _dateAndTime;};
+	public String getExtension () {return _ext;};
 
 
 	public void parse () throws Throwable {
+		
+		// Get file extension (should be .dat)
+		String[] parts = _fileName.split("\\.");
+		int l = parts.length;
+		_ext = parts[l-1];
+		
+		// Read file
 		InputStream in = new FileInputStream(_file);
 
 		// JVM Assumes Big Endian. Need to flip.
@@ -48,6 +61,7 @@ public class MRMetaData {
 
 		Integer nProtHeaderLen = din.readInt();
 		Integer nNrProtocols = din.readInt();
+		System.out.println("nProtocols="+nNrProtocols);
 		for (int i=0; i<nNrProtocols; i++) {
 
 			// Protocol Name
@@ -59,9 +73,9 @@ public class MRMetaData {
 				idx++;
 			}
 			String protocolName = new String(pcProtName, 0, idx);
-//			System.out.println("Protocol Name='"+protocolName +"'");
+			System.out.println("Protocol Name='"+protocolName +"'");
 			
-			// Read the protoco buffer
+			// Read the protocol buffer
 			Integer protocolLength = din.readInt();
 			byte[] header = new byte[protocolLength];
 			din.read(header, 0, protocolLength);
@@ -78,11 +92,16 @@ public class MRMetaData {
 				_patientID = find ("PatientID", stringHeader);
 				String patientName = find ("tPatientName", stringHeader);
 				parseName (patientName);
+				//
+				parseDate (find ("FrameOfReference", stringHeader));
 			}
 //			p.close();
 		}
 
 		din.close();
+		if (_patientID==null || _lastName==null || _dateAndTime==null) {
+			throw new Exception("Failed to extract all of patient ID, patient Name and Date from header.");
+		}
 	}
 
 	private String find (String thing, String header) throws Throwable {
@@ -99,6 +118,18 @@ public class MRMetaData {
 		return header.substring(start+3, end-3);
 	}
 
+	private void parseDate (String date) throws Throwable {
+		// Of the form  1.3.12.2.1107.5.2.34.18978.1.20140718161933906.0.0.0
+		if (date==null) return;
+		String[] parts = date.split("\\.");
+		int l = parts.length;
+		if (l==14) {
+			String t = parts[10];
+			String t2 = t.substring(0,13);
+			_dateAndTime = DateUtil.dateFromString(t2, "yyyyMMddHHmmss");
+		}
+	}
+
 	private void parseName (String name) {
 		String[] parts = name.split("^");
 		int l = parts.length;
@@ -110,7 +141,7 @@ public class MRMetaData {
 			_lastName = name;
 		}
 	}
-	public void print () {
+	public void print () throws Throwable {
 		System.out.println("  File name           = " + _fileName);
 		if (_firstName!=null && _lastName!=null) {
 			System.out.println("  Patient name        = " + _firstName + " " + _lastName);
@@ -119,6 +150,10 @@ public class MRMetaData {
 		}
 		if (_patientID != null) {
 			System.out.println("  Patient ID = " + _patientID);
+		}
+		if (_dateAndTime != null) {
+			String tDate = DateUtil.formatDate(_dateAndTime, true, false);
+			System.out.println("  Date = " + tDate);
 		}
 	}
 
